206639cee1a1e1b8e99dc6f7369e06380f515ed1
Bittu Panchal <115232340+BittuPanchal@users.noreply.github.com>
2024-06-24 11:57:13 +0530

    Update recording.py

--- recording.py
+++ recording.py
@@ -310,9 +310,10 @@ scrollable_content = f"""
   <style>
   .scrollable-container {{
     display: flex;
-    overflow: hidden;
     width: 400px; /* Adjust to the width of one section */
     height: 400px; /* Adjust to the height of one section */
+    overflow-x: auto;
+    white-space: nowrap;
     position: relative;
   }}
   
@@ -333,6 +334,8 @@ scrollable_content = f"""
     box-sizing: border-box; /* Include padding and border in the element's total width and height */
     height: 100%;
     overflow-y: auto; /* Enable vertical scrolling */
+    white-space: normal; /* Allow text to wrap */
+
   }}
   </style>
   
